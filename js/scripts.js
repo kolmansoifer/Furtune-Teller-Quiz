2efecfd64f3c083ec5081cc757dbc453b66b4ef8
Amanda Otero <mandystar018@gmail.com>
2020-08-18 17:32:18 -0700

    Edit the retunr value of the if else statements
    Co-authored-by:  Kolman Soifer <kolmansoifer@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -9,19 +9,23 @@ $(document).ready(function() {
       const lucky = $(this).val();
       arrayLucky.push(lucky);
     });
-
     $("input:checkbox[name=unlucky]:checked").each(function(){
       const unlucky = $(this).val();
       arrayUnlucky.push(unlucky);    
     });
-
-    if (lucky.length >= 3 && unlucky.length <= 3) {
-      $("#result3").text("Woohoo!  Nice to see things have gone your way recently.  Go out and buy a lottery ticket today!")
-    } else if (lucky.length <= 3 && unlucky.length >= 3) {
-      $("#result1").text("Yikes!  You've had some recent bad luck.  Please be careful this next week.")
-    } else {
-      $("#result2").text("Whew!  Things have been about average for you, luckwise...  Have a great week and watch out for black cats!")
+    function fortune(arrayLucky, arrayUnlucky){
+      let fortune;
+      if (arrayLucky.length >= 3 && arrayUnlucky.length <= 3) {
+        fortune = "Woohoo!  Nice to see things have gone your way recently.  Go out and buy a lottery ticket today!"
+      } else if (lucky.length <= 3 && unlucky.length >= 3) {
+        fortune = "Yikes!  You've had some recent bad luck.  Please be careful this next week."
+      } else {
+        fortune = "Whew!  Things have been about average for you, luckwise...  Have a great week and watch out for black cats!"
+      }
+    return fortune; 
     }
 
+    let newFortune =(arrayLucky, arrayUnlucky);
+    $(fortune).text(newFortune);
   });
-});
+})
